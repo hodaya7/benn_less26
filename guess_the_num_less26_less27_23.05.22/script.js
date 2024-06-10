714f5eea450829e50ebbbcff5725cabd627c1c95
hodaya7 <hodayabig@gmail.com>
2024-06-11 00:46:29 +0300

    guess num

--- guess_the_num_less26_less27_23.05.22/script.js
+++ guess_the_num_less26_less27_23.05.22/script.js
@@ -1,4 +1,4 @@
-//לא גמור כי לא הקליט לו בשיעור 28
+//לא גמור 
 //מספר בין אחד ל30 כל פעם שהאתר נטען
 function generateRandomNumber() {
     return Math.floor(Math.random() * 29 + 1);
@@ -23,11 +23,29 @@ function checkGuess(userGuess) {
 }
 
 document.querySelector(".check").addEventListener("click", function () {
+    if(document.querySelector(".guess").value!="")
     //דברים שיוזר מזין לא נשים בגלובל
     //console.log(document.querySelector(".guess").value);
     //הגיע כמחרוזת
     checkGuess(Number(document.querySelector(".guess").value));
+    else
+    document.querySelector(".message").textContent = "Please enter a number";
 });
+
+document.querySelector(".again").addEventListener("click", function () {
+    initGame();
+});
+
+function initGame() {
+    score = 10;
+    document.querySelector(".guess").value = "";
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".message").textContent = "You can guess now";
+    randomNumber = generateRandomNumber();
+    console.log(randomNumber);
+    document.querySelector(".right").classList.remove("right-correct");
+    reAddCheckButton();
+}
 
 function handleScore(resFromChkGuess) {
     switch (resFromChkGuess) {
@@ -36,24 +54,52 @@ function handleScore(resFromChkGuess) {
             handleCorrect();
             break;
         case "your guess is smaller than the random":
-            if (score > 1)
+            if (score > 1) {
                 document.querySelector(".message").textContent = "your guess is smaller than the random";
-            else
+                score--;
+                document.querySelector(".score").textContent = score;
+            }
+
+            else {
                 document.querySelector(".message").textContent = "you lost";
-            score--;
-            document.querySelector(".score").textContent = score;
+                score--;
+                document.querySelector(".score").textContent = score;
+                removeCheckButton();
+            }
             break;
         case "your guess is bigger than the random":
-            if (score > 1)
+            if (score > 1) {
                 document.querySelector(".message").textContent = "your guess is bigger than the random";
-            else
+                score--;
+                document.querySelector(".score").textContent = score;
+            }
+            else {
                 document.querySelector(".message").textContent = "you lost";
-            score--;
-            document.querySelector(".score").textContent = score;
+                score--;
+                document.querySelector(".score").textContent = score;
+                removeCheckButton();
+            }
             break;
     }
 }
 
 function handleCorrect() {
     document.querySelector(".right").classList.add("right-correct");
+    handleHighScore();
+    removeCheckButton();
+}
+
+//less30
+function handleHighScore() {
+    if (score > highScore)
+        highScore = score;
+    document.querySelector(".highscore").textContent = highScore;
+}
+
+function removeCheckButton() {
+    document.querySelector(".check").classList.add("not-display-element");
+}
+
+function reAddCheckButton() {
+    document.querySelector(".check").classList.remove("not-display-element");
 }
